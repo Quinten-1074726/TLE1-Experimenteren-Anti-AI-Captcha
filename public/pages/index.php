f9b8d9224610b7c37e9b3c81019294e32e97e4aa
HiddeBouwman <bouwmanhidde@gmail.com>
2025-09-22 14:20:59 +0200

    css opgeschoont

--- public/pages/index.php
+++ public/pages/index.php
@@ -7,19 +7,10 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link rel="stylesheet" href="styling/index.css">
-    <script src="javascript/index.js"></script>
-    <!-- font -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=SUSE+Mono:ital,wght@0,100..800;1,100..800&display=swap"
-        rel="stylesheet">
+    <?php include "../partials/defaultsettings.php" ?>
+    <link rel="stylesheet" href="../styling/index.css">
+    <script src="../javascript/index.js"></script>
 </head>
 
 
@@ -29,7 +20,7 @@
 
 <body>
 
-    <?php include "partials/header.php" ?>
+    <?php include "../partials/header.php" ?>
 
     <main>
         <!-- left -->
--- public/pages/register.php
+++ public/pages/register.php
@@ -49,9 +49,8 @@ if (isset($_POST['submit'])) {
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>registreren</title>
+    <?php include "../partials/defaultsettings.php" ?>
     <link rel="stylesheet" href="../styling/crud.css">
 </head>
 <body>

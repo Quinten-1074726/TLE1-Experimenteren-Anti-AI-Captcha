89df9ccea92b34194e3fa41d8f1c6a1793e6c9fa
lisa-mao <1091575@hr.nl>
2025-09-24 13:41:29 +0200

    channel page html css is af

--- public/channel.php
+++ public/channel.php
@@ -8,13 +8,23 @@ $password = "";
 $db = mysqli_connect($host, $user, $password, $database)
 or die("Error: " . mysqli_connect_error());
 
+////
+//$id = $_GET['id'];
+//
+//$query = "SELECT * FROM users WHERE id = $id";
+//
+//$result = mysqli_query($db, $query);
+//$users = mysqli_fetch_assoc($result);
+//mysqli_close($db);
 // required when working with sessions
 session_start();
 
 // Redirect if already logged in
 if (isset($_SESSION['loggedIn']) && $_SESSION['loggedIn'] === true) {
     $redirectUrl = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : 'index.php';
-    header("Location: $redirectUrl");
+
+} else {
+    header("Location: 'index.php'");
     exit;
 }
 ?>
@@ -26,22 +36,33 @@ if (isset($_SESSION['loggedIn']) && $_SESSION['loggedIn'] === true) {
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<link rel="stylesheet" href="styling/channel.css">
+    <link rel="stylesheet" href="styling/channel.css">
     <link rel="stylesheet" href="styling/index.css">
     <script src="javascript/channel.js" defer></script>
     <title>Channel page</title>
 </head>
 <body id="body">
 
-    <?php include "header.php" ?>
-<div class="divContainer">
+<?php include "header.php" ?>
 
+<div class="divContainer">
     <div class="left_side">
         <div>
-            <a>AI Filter</a>
-            <a>Home</a>
+            <div>
+                <label id="ai_filter_label">
+                    AI Filter
+                    <span class="switch">
+                            <input type="checkbox">
+                            <span class="slider round"></span>
+                        </span>
+                </label>
+            </div>
+
+            <a href="index.php">Home</a>
             <a href="trending.php">Trending</a>
             <a>Subcriptions</a>
+            <a href="channel.php">My channel</a>
+            <a href="upload.php" class="btn">Video uploaden</a>
         </div>
         <div>
             <!-- channels here -->
@@ -50,49 +71,57 @@ if (isset($_SESSION['loggedIn']) && $_SESSION['loggedIn'] === true) {
 
         </div>
     </div>
-    <!-- right -->
-    <div class="right_side">
-        <!-- script -->
+
+
+    <div class="emptyContainer">
+
     </div>
 
-<div class="emptyContainer">
+    <section id="right">
+        <div class="rightDiv">
+            <div class="banner">
+                <img class="imageBanner" src="images/banner.png">
+            </div>
+            <div class="profile">
+                <div style="display: flex; flex-direction: row;">
+                    <div class="profilePicture">
+                        <img style="scale: 1.3" src="images/profile.png">
+                    </div>
+                    <div class="channelDetails">
+                        <div class="channelName">
+                            <h2>Channel name</h2>
+                        </div>
 
-</div>
 
-<section id="right">
-    <div class="rightDiv">
-        <div class="banner">
-            <img class="imageBanner" src="images/banner.png">
-        </div>
-        <div class="profile">
-            <div style="display: flex; flex-direction: row">
-                <div class="profilePicture">
-                    <img style="scale: 1.3" src="images/profile.png">
-                </div>
-                <div class="channelDetails">
-                    <div class="channelName">
-                        <h2>Channel name</h2>
+                        <div class="amounts">
+                            <p>
+                                subscribers count || a/o video's
+                            </p>
+
+                            <div class="description">
+
+                                    Welcome to my channel!!!, jkcbahjfbahjdashdjbsahdjasbdskdj
+
+                            </div>
+
+
+
+                        </div>
                     </div>
                 </div>
             </div>
-                <div class="amounts">
-                    <p>
-                        subscribercount
-                    </p>
-                    <p>
-                        a/o videos
-                    </p>
+
+            <div class="lijntje">
+            </div>
+
+            <div class="overviewVideos">
+                <div class="video1 ">
+
                 </div>
             </div>
         </div>
-        <div class="overviewVideos">
-            <div class="video1 ">
+    </section>
 
-            </div>
-        </div>
-    </div>
-</section>
-
-</div>
+    <!--</div>-->
 </body>
 </html>

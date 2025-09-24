2b4571c4ef17977c6cd91715b3fe197785f31c26
Quinten-1074726 <1074726@hr.nl>
2025-09-24 10:22:18 +0200

    map fix

--- public/database/connection.php
+++ public/database/connection.php
@@ -2,7 +2,7 @@
 $host = "127.0.0.1";
 $database = "tle1-2";
 $user = "root";
-$password = "";
+$password = "root";
 
 $db = mysqli_connect($host, $user, $password, $database)
 or die("Error: " . mysqli_connect_error());

bb870c3db316a1b25ee2e205c42fcc0b256ea82d
HiddeBouwman <bouwmanhidde@gmail.com>
2025-10-01 17:30:10 +0200

    node weggehaald

--- public/database/connection.php
+++ public/database/connection.php
@@ -1,8 +1,8 @@
 <?php
 $host = "127.0.0.1";
-$database = "tle1";
-$user = "root";
-$password = "";
+$database = "prj_2025_2026_tle1_t5";
+$user = "prj_2025_2026_tle1_t5";
+$password = "rivaisha";
 
 $db = mysqli_connect($host, $user, $password, $database)
 or die("Error: " . mysqli_connect_error());
